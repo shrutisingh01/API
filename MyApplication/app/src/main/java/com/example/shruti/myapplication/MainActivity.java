5dd4b6cc5319badeca45e5a654eaf88abad89fe0
shrutisingh01 <shrutisingh1996@gmail.com>
2018-08-21 16:55:44 +0530

    ss

---
+++ MyApplication/app/src/main/java/com/example/shruti/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.shruti.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button click;
+    public static TextView data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.button);
+        data = (TextView) findViewById(R.id.fetcheddata);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FetchData process = new FetchData();
+                process.execute();
+            }
+        });
+
+    }
+}
